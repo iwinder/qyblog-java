3ee20e82798d7a1e800e962143843223c1652153
windCoder <>
2018-11-13 23:38:36 +0800

    调整发布

--- qycms-core/common/src/main/java/com/windcoder/qycms/QycmsApplication.java
+++ qycms-core/common/src/main/java/com/windcoder/qycms/QycmsApplication.java
@@ -2,6 +2,8 @@ package com.windcoder.qycms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
@@ -12,8 +14,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  */
 @SpringBootApplication
 @EnableJpaAuditing
-public class QycmsApplication {
+public class QycmsApplication  extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(QycmsApplication.class,args);
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(QycmsApplication.class);
+    }
+
 }
