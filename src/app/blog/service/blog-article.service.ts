b988d945ca32b868e84d9dd834235e705ebcc3ec
zhangrucong <zhangrucong@parim.net>
2018-11-15 15:38:09 +0800

    调整接口url

--- src/app/blog/service/blog-article.service.ts
+++ src/app/blog/service/blog-article.service.ts
@@ -7,7 +7,7 @@ import { FormDataUtil } from '../../core/utils/form-data-util';
 
 @Injectable()
 export class BlogArticleService {
-    url: string  = 'api/web/articles';
+    url: string  = '/api/web/articles';
     constructor(private http: HttpClient) { }
 
     findAll( params?): Observable<Page<BlogArticle>> {
