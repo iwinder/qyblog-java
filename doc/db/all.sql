8b83af492e84643ddd28398483fdf82b98c65016
windcoder <windcoderz@foxmail.com>
2020-07-09 23:35:41 +0800

    添加授权相关sql

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -21,6 +21,7 @@ create table `sys_user` (
 # INSERT INTO `sys_user` (`id`, `created_date`, `last_modified_date`, `avatar`, `email`, `deleted`, `disable`, `nickname`, `password`, `salt`, `username`, `created_by`, `last_modified_by`) VALUES (1, '2018-9-7 03:14:53', '2018-11-4 03:58:11', '/content/upload/2018/11/04/e246ba56-81aa-4226-8189-cc2d10ccd56a.jpg', '1@qq.com', 0, 0, '测试', 'e68b0a6751860afed4938951be1ef002ee697d04', 'b8314a417d4037731ad10f92c25dc911fad829094a22', 'admin', 1, 1);
 INSERT INTO `sys_user` (`id`, `created_date`, `last_modified_date`, `avatar`, `email`, `deleted`, `disable`, `nickname`, `password`, `salt`, `username`, `created_by`, `last_modified_by`) VALUES (1, now(), now(), '/content/upload/2018/11/04/e246ba56-81aa-4226-8189-cc2d10ccd56a.jpg', '1@qq.com', 0, 0, '测试', 'e68b0a6751860afed4938951be1ef002ee697d04', 'b8314a417d4037731ad10f92c25dc911fad829094a22', 'admin', 1, 1);
 
+select min(id) into @v_system_user_id from sys_user;
 
 -- 文章
 drop table if exists `blog_article`;
@@ -110,3 +111,133 @@ INSERT INTO blog_category (id,name,description,key_word,id_path,name_path,displa
 (1,'默认分类',NULL,NULL,'1','默认分类',1,NULL,0,NULL,NULL,'2020-06-30 04:02:17.0','2020-06-30 04:02:17.0');
 INSERT INTO blog_category_tree (parent_id,child_id,distance,child_count) VALUES
 (1,1,0,1);
+
+
+
+
+-- 资源
+drop table if exists `sys_privilege`;
+CREATE TABLE `sys_privilege` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT comment 'id',
+  `name` varchar(255) DEFAULT NULL comment '资源名称',
+  `url` varchar(255) DEFAULT NULL comment '资源路径',
+  `description` varchar(255) DEFAULT NULL comment '描述，界面UI显示字段',
+  `identifier` varchar(255) DEFAULT NULL comment '权限字符串',
+  `availabled` bit(1) DEFAULT 1 comment '是否可用,如果不可用将不会添加给用户 0 不可用，1 可用',
+  `hided` bit(1) DEFAULT 0 comment '是否隐藏, 0 不隐藏，1 隐藏',
+  `parent_id` bigint(20) DEFAULT NULL comment '父级 id',
+  `type` varchar(255) DEFAULT NULL comment '类型',
+  `created_by` bigint(20) DEFAULT null comment '创建者',
+  `last_modified_by` bigint(20) DEFAULT NULL comment '更新者',
+  `created_date` datetime DEFAULT now() comment '创建时间',
+  `last_modified_date` datetime DEFAULT now() comment '更新时间',
+  PRIMARY KEY (`id`),
+  KEY `FK7h567kbm8i2uteel1pwveyamj` (`parent_id`),
+  CONSTRAINT `FK7h567kbm8i2uteel1pwveyamj` FOREIGN KEY (`parent_id`) REFERENCES `sys_privilege` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='资源';
+
+
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('SYSTEM','系统管理','系统管理',null);
+select id into @parent_id from sys_privilege where IDENTIFIER='SYSTEM';
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('SYSTEM:PERMISSION','授权管理','授权管理',@parent_id);
+select id into @c_parent_id from sys_privilege where IDENTIFIER='SYSTEM:PERMISSION';
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('SYSTEM:PERMISSION:VIEW','查看授权','查看授权',@c_parent_id);
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('SYSTEM:PERMISSION:ADD','添加授权','添加授权',@c_parent_id);
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('SYSTEM:PERMISSION:EDIT','修改授权','修改授权',@c_parent_id);
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('SYSTEM:PERMISSION:REMOVE','删除授权','删除授权',@c_parent_id);
+
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('SYSTEM:ROLE','角色管理','角色管理',@parent_id);
+select id into @c_parent_id from sys_privilege where IDENTIFIER='SYSTEM:ROLE';
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('SYSTEM:ROLE:VIEW','查看角色','查看角色',@c_parent_id);
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('SYSTEM:ROLE:ADD','添加角色','添加角色',@c_parent_id);
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('SYSTEM:ROLE:EDIT','修改角色','修改角色',@c_parent_id);
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('SYSTEM:ROLE:REMOVE','删除角色','删除角色',@c_parent_id);
+
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('SYSTEM:USER','用户管理','用户管理',@parent_id);
+select id into @c_parent_id from sys_privilege where IDENTIFIER='SYSTEM:USER';
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('SYSTEM:USER:VIEW','查看用户','查看用户',@c_parent_id);
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('SYSTEM:USER:ADD','添加用户','添加用户',@c_parent_id);
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('SYSTEM:USER:EDIT','修改用户','修改用户',@c_parent_id);
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('SYSTEM:USER:REMOVE','删除用户','删除用户',@c_parent_id);
+
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('BLOG','博客管理','博客管理',null);
+select id into @parent_id from sys_privilege where IDENTIFIER='BLOG';
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('BLOG:ARTICLE','文章管理','文章管理',@parent_id);
+select id into @c_parent_id from sys_privilege where IDENTIFIER='BLOG:ARTICLE';
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('BLOG:ARTICLE:VIEW','查看文章','查看文章',@c_parent_id);
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('BLOG:ARTICLE:ADD','添加文章','添加文章',@c_parent_id);
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('BLOG:ARTICLE:EDIT','修改文章','修改文章',@c_parent_id);
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('BLOG:ARTICLE:REMOVE','删除文章','删除文章',@c_parent_id);
+
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('BLOG:CATEGORY','分类管理','分类管理',@parent_id);
+select id into @c_parent_id from sys_privilege where IDENTIFIER='BLOG:CATEGORY';
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('BLOG:CATEGORY:VIEW','查看分类','查看分类',@c_parent_id);
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('BLOG:CATEGORY:ADD','添加分类','添加分类',@c_parent_id);
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('BLOG:CATEGORY:EDIT','修改分类','修改分类',@c_parent_id);
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('BLOG:CATEGORY:REMOVE','删除分类','删除分类',@c_parent_id);
+
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('BLOG:TAG','标签管理','标签管理',@parent_id);
+select id into @c_parent_id from sys_privilege where IDENTIFIER='BLOG:TAG';
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('BLOG:TAG:VIEW','查看标签','查看标签',@c_parent_id);
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('BLOG:TAG:ADD','添加标签','添加标签',@c_parent_id);
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('BLOG:TAG:EDIT','修改标签','修改标签',@c_parent_id);
+Insert into sys_privilege (IDENTIFIER,NAME,description,PARENT_ID) values ('BLOG:TAG:REMOVE','删除标签','删除标签',@c_parent_id);
+
+
+
+
+-- qycmsdev.sys_role definition
+drop table if exists `sys_role`;
+CREATE TABLE `sys_role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT comment 'id',
+  `name` varchar(255) DEFAULT NULL comment '名称',
+  `role_type` varchar(255) DEFAULT NULL comment '角色类型',
+  `remark` varchar(255) DEFAULT NULL comment '备注',
+  `created_by` bigint(20) DEFAULT null comment '创建者',
+  `last_modified_by` bigint(20) DEFAULT NULL comment '更新者',
+  `created_date` datetime DEFAULT now() comment '创建时间',
+  `last_modified_date` datetime DEFAULT now() comment '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='角色';
+
+
+Insert into sys_role (ID,NAME,ROLE_TYPE,CREATED_BY,LAST_MODIFIED_BY) values (@v_superuser_role_id,'超级管理员','SYSTEM',@v_system_user_id,@v_system_user_id);
+
+
+-- qycmsdev.sys_role_privilege definition
+drop table if exists `sys_role_privilege`;
+CREATE TABLE `sys_role_privilege` (
+  `role_id` bigint(20) NOT NULL comment '角色id',
+  `privilege_id` bigint(20) NOT NULL comment '资源id',
+  PRIMARY KEY (`role_id`,`privilege_id`),
+  KEY `FKvf9lt66q9oyjludcg83nfinq` (`privilege_id`),
+  CONSTRAINT `FKr3sw06rqbrufgrkfq2c8dxhvb` FOREIGN KEY (`role_id`) REFERENCES `sys_role` (`id`),
+  CONSTRAINT `FKvf9lt66q9oyjludcg83nfinq` FOREIGN KEY (`privilege_id`) REFERENCES `sys_privilege` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='角色权限表';
+
+select min(id) into @v_superuser_role_id from sys_role;
+insert into sys_role_privilege (role_id, privilege_id)
+select @v_superuser_role_id as role_id, p.id as privilege_id from sys_privilege p;
+
+
+-- qycmsdev.sys_privilege definition
+drop table if exists `sys_permission`;
+CREATE TABLE `sys_permission` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) DEFAULT NULL comment '用户',
+  `role_id` bigint(20) DEFAULT NULL comment '角色',
+  `privilege_id` bigint(20) DEFAULT NULL comment '权限',
+  `created_by` bigint(20) DEFAULT null comment '创建者',
+  `last_modified_by` bigint(20) DEFAULT NULL comment '更新者',
+  `created_date` datetime DEFAULT now() comment '创建时间',
+  `last_modified_date` datetime DEFAULT now() comment '更新时间',
+  PRIMARY KEY (`id`),
+  KEY `FKl0lymq83eg69a9awoqn9sp3pi` (`privilege_id`),
+  KEY `FKhidmju68461bmv7st3pfkrhcu` (`role_id`),
+  KEY `FK1nk6vdhuhcniyse9wik3pp5l7` (`user_id`),
+  CONSTRAINT `FK1nk6vdhuhcniyse9wik3pp5l7` FOREIGN KEY (`user_id`) REFERENCES `sys_user` (`id`),
+  CONSTRAINT `FKhidmju68461bmv7st3pfkrhcu` FOREIGN KEY (`role_id`) REFERENCES `sys_role` (`id`),
+  CONSTRAINT `FKl0lymq83eg69a9awoqn9sp3pi` FOREIGN KEY (`privilege_id`) REFERENCES `sys_privilege` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='授权表';
+# select min(id) into @v_system_user_id from sys_user;
+Insert into sys_permission (CREATED_BY,LAST_MODIFIED_BY,PRIVILEGE_ID,ROLE_ID,USER_ID) values (@v_system_user_id,@v_system_user_id,null,@v_superuser_role_id,@v_system_user_id);
