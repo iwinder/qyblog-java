c433c3dc87f351152b1ddcbce818da2e60b327c4
蜜汁炒酸奶 <windcoderz@foxmail.com>
2023-09-27 22:37:08 +0800

    refactor(common): 增加空分支，用作分支合并时创建新分支的分支模板

--- qycms/qycms-core/system-provider/src/main/java/com/windcoder/qycms/system/dto/SysIpBlackPageDto.java
+++
@@ -1,9 +0,0 @@
-package com.windcoder.qycms.system.dto;
-
-import com.windcoder.qycms.dto.PageDto;
-import lombok.Data;
-
-@Data
-public class SysIpBlackPageDto extends PageDto {
-    private String searchText;
-}
